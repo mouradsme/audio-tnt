a11319373ab20a0afa452c9b39d4e3b5f761a10e
mouradsme <mouradsme@gmail.com>
2025-04-21 21:02:54 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,9 @@ use App\Http\Controllers\AudioTranscriptionController;
 use App\Http\Controllers\TranslationController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/audio_listener_1', function () {
     return view('audio_listener_test');
 });
 
-Route::get('/audio_listener_2', function () {
-    return view('audio_listener_test');
-});
 
 Route::post('/transcribe', [AudioTranscriptionController::class, 'transcribe']);
 Route::post('/translate', [TranslationController::class, 'translate']);
